5d745522fac371f3a67f45db042d4edffb3a0b64
Max Mantilla Rodríguez <mmantilla@bsginstitute.com>
2022-08-02 11:14:32 -0500

    Actualización

---
+++ src/app/Public/FormularioPublicidad/formulario-publicidad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-publicidad',
+  templateUrl: './formulario-publicidad.component.html',
+  styleUrls: ['./formulario-publicidad.component.scss']
+})
+export class FormularioPublicidadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
