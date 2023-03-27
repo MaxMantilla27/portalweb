c4a03b42d824973c6b062421df56fc1213bdef17
Griselberto Huaman Condori <ghuamanc@bsginstitute.com>
2023-03-27 15:21:01 -0500

    Form IziPay

--- src/app/aula-virtual/confirmacion-pago-izipay/confirmacion-pago-izipay.component.ts
+++ src/app/aula-virtual/confirmacion-pago-izipay/confirmacion-pago-izipay.component.ts
@@ -59,25 +59,30 @@ export class ConfirmacionPagoIzipayComponent
 
   customForm(){
     var boton=document.getElementsByClassName('kr-popin-button');
-   boton[0].setAttribute("style",
-   "background-color: #F8893F;color: white;padding: 0 6px 0 6px;margin: 6px 8px 6px 8px;min-width: 88px;border-radius: 3px;font-size: 14px;"+
-   "text-align: center;text-transform: uppercase;text-decoration:none;border: none;outline: none;box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26);&:hover {background-color: #b26a3b}"
-   )
-   var botonPago =document.getElementsByClassName('kr-payment-button');
-   botonPago[0].setAttribute("style",
-   "background-color: #F8893F;padding: 0 6px 0 6px;margin: 6px 8px 6px 8px;min-width: 88px;border-radius: 3px;font-size: 14px;"+
-   "text-align: center;text-transform: uppercase;text-decoration:none;border: none;outline: none;box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26);&:hover {background-color: #b26a3b}"
-   )
+    if(typeof(boton) != 'undefined' && boton != null && boton.length>0)
+      boton[0].setAttribute("style",
+      "background-color: #F8893F;color: white;padding: 0 6px 0 6px;margin: 6px 8px 6px 8px;min-width: 88px;border-radius: 3px;font-size: 14px;"+
+      "text-align: center;text-transform: uppercase;text-decoration:none;border: none;outline: none;box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26);&:hover {background-color: #b26a3b}"
+      );
+
+    var botonPago =document.getElementsByClassName('kr-payment-button');
+    if(typeof(botonPago) != 'undefined' && botonPago != null && botonPago.length>0)
+      botonPago[0].setAttribute("style",
+      "background-color: #F8893F;padding: 0 6px 0 6px;margin: 6px 8px 6px 8px;min-width: 88px;border-radius: 3px;font-size: 14px;"+
+      "text-align: center;text-transform: uppercase;text-decoration:none;border: none;outline: none;box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26);&:hover {background-color: #b26a3b}"
+      );
+
     var logo=document.getElementsByClassName('kr-header-logo');
-    logo[1].setAttribute("src",
-    "../../../assets/imagenes/logo-bsg.png")
+    if(typeof(logo) != 'undefined' && logo != null && logo.length>0) 
+      logo[1].setAttribute("src","../../../assets/imagenes/logo-bsg.png");
 
     var spanNombre= document.getElementsByClassName('kr-popin-shop-name');
-    spanNombre[0].className="";
+    if(typeof(spanNombre) != 'undefined' && spanNombre != null && spanNombre.length>0) 
+      spanNombre[0].className="";
 
     var modalHeader = document.getElementsByClassName('kr-popin-modal-header');
-    modalHeader[0].setAttribute("style",
-    " margin-bottom: 0px;height: 70px;")
+    if(typeof(modalHeader) != 'undefined' && modalHeader != null && modalHeader.length>0) 
+      modalHeader[0].setAttribute("style", "margin-bottom: 0px;height: 70px;");
     
     this.hidenBotom=false
   }
