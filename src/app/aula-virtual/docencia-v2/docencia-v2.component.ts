9626505682f164601d4bcab1122c713c18df06c6
Miguel Mora <mmora@bsginstitute.com>
2023-06-01 16:25:49 -0500

    cambios

--- src/app/aula-virtual/docencia-v2/docencia-v2.component.ts
+++ src/app/aula-virtual/docencia-v2/docencia-v2.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatTabChangeEvent } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-docencia-v2',
   templateUrl: './docencia-v2.component.html',
-  styleUrls: ['./docencia-v2.component.scss']
+  styleUrls: ['./docencia-v2.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class DocenciaV2Component implements OnInit {
 
