59bee0e66b75e6a2d9d86760d472f0177ff41a61
Max Mantilla Rodríguez <mmantilla@bsginstitute.com>
2024-10-09 10:38:39 -0500

    niubiz

--- src/app/aula-virtual/pago/pago-medio-pago/detalle-pago-internacional-niubiz/detalle-pago-internacional-niubiz.component.ts
+++ src/app/aula-virtual/pago/pago-medio-pago/detalle-pago-internacional-niubiz/detalle-pago-internacional-niubiz.component.ts
@@ -19,38 +19,17 @@ import { SessionStorageService } from 'src/app/Core/Shared/Services/session-stor
 import { environment } from 'src/environments/environment';
 import { MedioPagoActivoPasarelaService } from 'src/app/Core/Shared/Services/MedioPagoActivoPasarela/medio-pago-activo-pasarela.service';
 import { ImagenTarjetas } from 'src/app/Core/Shared/ImagenTarjetas';
+
 @Component({
   selector: 'app-detalle-pago-internacional-niubiz',
   templateUrl: './detalle-pago-internacional-niubiz.component.html',
   styleUrls: ['./detalle-pago-internacional-niubiz.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class DetallePagoInternacionalNiubizComponent implements OnInit, OnDestroy, AfterViewChecked
-{
-  private signal$ = new Subject();
+export class DetallePagoInternacionalNiubizComponent implements OnInit, OnDestroy, AfterViewChecked {
+  private signal$ = new Subject<void>();
   public resultNiubiz: any;
-  public jsonSave: RegistroProcesoPagoAlumnoDTO = {
-    IdentificadorTransaccion: '',
-    MedioCodigo: '',
-    MedioPago: '',
-    RequiereDatosTarjeta: true,
-    TransactionToken: '',
-    Estado: null,
-    Comprobante: false,
-    CodigoTributario: '',
-    RazonSocial: '',
-    IdPasarelaPago: 0,
-    PagoPSE: false,
-    TarjetaHabiente: {
-      Aniho: '',
-      CodigoVV: '',
-      Mes: '',
-      NumeroDocumento: '',
-      NumeroTarjeta: '',
-      Titular: '',
-      fecha: '',
-    },
-  };
+  public jsonSave: RegistroProcesoPagoAlumnoDTO = this.inicializarJsonSave();
   public dialogRef: any;
   public IdMatriculaCabecera = 0;
   public json: RegistroRespuestaPreProcesoPagoDTO = {
@@ -59,10 +38,12 @@ export class DetallePagoInternacionalNiubizComponent implements OnInit, OnDestro
   };
   public url = '/AulaVirtual/PagoExitoso/';
   public urlBase = environment.url_portal;
-  private kryptonScriptLoaded: boolean = false;
-  public CompletamenteCargado: boolean = false; // Nueva variable
-  public DataComprobante: any = {}; // Inicialización de DataComprobante
-  public IdPasarelaPago=0;
+  public kryptonScriptLoaded = false;
+  public CompletamenteCargado = false;
+  public DataComprobante: any = {};
+  public IdPasarelaPago = 0;
+  public tarjetas: any;
+
   constructor(
     private _ActivatedRoute: ActivatedRoute,
     private _FormaPagoService: FormaPagoService,
@@ -73,22 +54,18 @@ export class DetallePagoInternacionalNiubizComponent implements OnInit, OnDestro
     private _MedioPagoActivoPasarelaService: MedioPagoActivoPasarelaService,
     private _t: ImagenTarjetas,
   ) {}
-  public tarjetas: any;
 
   ngOnInit(): void {
     this._ActivatedRoute.params.pipe(takeUntil(this.signal$)).subscribe({
-      next: (x) => {
-        this.IdMatriculaCabecera = parseInt(x['idmatricula']);
-        this.IdPasarelaPago = parseInt(x['idpasarelapago']);
-        this.json.IdentificadorTransaccion = x['identificador'];
-        const r = this._SessionStorageService.SessionGetValue(
-          this.json.IdentificadorTransaccion
-        );
-        this.json.RequiereDatosTarjeta = r === 'false' ? false : true;
+      next: (params) => {
+        this.IdMatriculaCabecera = +params['idmatricula'];
+        this.IdPasarelaPago = +params['idpasarelapago'];
+        this.json.IdentificadorTransaccion = params['identificador'];
+        this.json.RequiereDatosTarjeta = this._SessionStorageService.SessionGetValue(this.json.IdentificadorTransaccion) !== 'false';
 
         this.url += this.json.IdentificadorTransaccion;
         this.ObtenerPreProcesoPagoCuotaAlumno();
-        this.ObtenerTarjetasMedioPago()
+        this.ObtenerTarjetasMedioPago();
       },
     });
   }
@@ -98,62 +75,90 @@ export class DetallePagoInternacionalNiubizComponent implements OnInit, OnDestro
       const visaElement = this._document.getElementById('visa');
       if (visaElement) {
         this.CompletamenteCargado = true;
-        this.loadVisanetCheckoutScript(); // Cargar el script de Visa Checkout
+        this.loadVisanetCheckoutScript();
       }
     }
   }
 
   ngOnDestroy(): void {
-    this.signal$.next(true);
+    this.signal$.next();
     this.signal$.complete();
+    const visaScript = this._document.getElementById('visa');
+    if (visaScript) {
+      this._renderer2.removeChild(this._document.body, visaScript);
+    }
   }
 
-  ObtenerPreProcesoPagoCuotaAlumno() {
+  private inicializarJsonSave(): RegistroProcesoPagoAlumnoDTO {
+    return {
+      IdentificadorTransaccion: '',
+      MedioCodigo: '',
+      MedioPago: '',
+      RequiereDatosTarjeta: true,
+      TransactionToken: '',
+      Estado: null,
+      Comprobante: false,
+      CodigoTributario: '',
+      RazonSocial: '',
+      IdPasarelaPago: 0,
+      PagoPSE: false,
+      TarjetaHabiente: {
+        Aniho: '',
+        CodigoVV: '',
+        Mes: '',
+        NumeroDocumento: '',
+        NumeroTarjeta: '',
+        Titular: '',
+        fecha: '',
+      },
+    };
+  }
+
+  ObtenerPreProcesoPagoCuotaAlumno(): void {
     this._FormaPagoService
       .ObtenerPreProcesoPagoCuotaAlumno(this.json)
       .pipe(takeUntil(this.signal$))
       .subscribe({
-        next: (x) => {
-          this.resultNiubiz = x._Repuesta;
-          console.log(this.resultNiubiz)
+        next: (response) => {
+          this.resultNiubiz = response._Repuesta;
+          console.log(this.resultNiubiz);
           if (this.resultNiubiz.estadoOperacion.toLowerCase() !== 'sent') {
-            this._router.navigate(['/AulaVirtual/MisCursos/' + this.IdMatriculaCabecera]);
+            this._router.navigate(['/AulaVirtual/MisCursos/', this.IdMatriculaCabecera]);
           }
-          this.resultNiubiz.total = 0;
-          this.resultNiubiz.listaCuota.forEach((l: any, index: number) => {
-            if (index === 0) {
-              this.resultNiubiz.total += l.cuotaTotal;
-            }
-          });
-          this.jsonSave = {
-            ...this.jsonSave,
-            IdentificadorTransaccion: this.json.IdentificadorTransaccion,
-            MedioCodigo: this.resultNiubiz.medioCodigo,
-            MedioPago: this.resultNiubiz.medioPago,
-            RequiereDatosTarjeta: this.json.RequiereDatosTarjeta,
-            TransactionToken: this.resultNiubiz.procesoPagoBotonVisa.transactionToken,
-          };
+          this.resultNiubiz.total = this.resultNiubiz.listaCuota[0]?.cuotaTotal || 0;
+          this.actualizarJsonSave();
         },
       });
   }
 
-  loadVisanetCheckoutScript() {
-    if (!this.kryptonScriptLoaded) {
-      const script = this._renderer2.createElement('script');
-      script.type = 'text/javascript';
-      script.src = 'https://static-content.vnforapps.com/v2/js/checkout.js';
-      script.onload = () => {
-        this.kryptonScriptLoaded = true;
-        console.log('Script de Visa Checkout cargado con éxito.');
-      };
-      script.onerror = () => {
-        console.error('Error al cargar el script de Visa Checkout.');
-      };
-      this._renderer2.appendChild(this._document.body, script);
-    }
+  private actualizarJsonSave(): void {
+    this.jsonSave = {
+      ...this.jsonSave,
+      IdentificadorTransaccion: this.json.IdentificadorTransaccion,
+      MedioCodigo: this.resultNiubiz.medioCodigo,
+      MedioPago: this.resultNiubiz.medioPago,
+      RequiereDatosTarjeta: this.json.RequiereDatosTarjeta,
+      TransactionToken: this.resultNiubiz.procesoPagoBotonVisa.transactionToken,
+    };
   }
 
-  openForm() {
+  loadVisanetCheckoutScript(): void {
+    if (this.kryptonScriptLoaded) return;
+
+    const script = this._renderer2.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://static-content.vnforapps.com/vToken/js/checkout.js';
+    script.onload = () => {
+      this.kryptonScriptLoaded = true;
+      console.log('Script de Visa Checkout cargado con éxito.');
+    };
+    script.onerror = () => {
+      console.error('Error al cargar el script de Visa Checkout.');
+    };
+    this._renderer2.appendChild(this._document.body, script);
+  }
+
+  openForm(): void {
     this.DataComprobante.idComprobante = this.jsonSave.Comprobante ? 1 : 2;
     this.DataComprobante.nroDoc = this.jsonSave.Comprobante
       ? this.jsonSave.CodigoTributario
@@ -161,82 +166,61 @@ export class DetallePagoInternacionalNiubizComponent implements OnInit, OnDestro
     this.DataComprobante.razonSocial = this.jsonSave.Comprobante ? this.jsonSave.RazonSocial : '';
 
     this._SessionStorageService.SessionSetValue('comprobante', JSON.stringify(this.DataComprobante));
-    if (!this.kryptonScriptLoaded) {
-      console.error('El script de Visa Checkout no se ha cargado aún.');
-      return;
-    }
 
-    if (typeof VisanetCheckout === 'undefined') {
-      console.error('VisanetCheckout no está disponible. Asegúrate de que el script se haya cargado correctamente.');
-      return;
-    }
-
-    // Verifica si la función configure existe
-    if (typeof VisanetCheckout.configure !== 'function') {
-      console.error('La función configure no está disponible en VisanetCheckout.');
+    if (!this.kryptonScriptLoaded || typeof VisanetCheckout === 'undefined' || typeof VisanetCheckout.configure !== 'function') {
+      console.error('El script o la configuración de Visa Checkout no están disponibles.');
       return;
     }
 
     try {
       VisanetCheckout.configure({
         sessiontoken: this.resultNiubiz.procesoPagoBotonVisa.sessionKey,
-        channel:  `paycard`,
+        channel: 'web',
         merchantid: this.resultNiubiz.procesoPagoBotonVisa.merchanId,
-        purchasenumber: 'web',
-        amount: parseFloat(this.resultNiubiz.procesoPagoBotonVisa.amount + '.00'),
+        purchasenumber: 'BSG Institute',
+        amount: parseFloat(`${this.resultNiubiz.procesoPagoBotonVisa.amount}.00`),
         expirationminutes: '5',
         timeouturl: `${this.urlBase}/AulaVirtual/MisPagos/${this.IdMatriculaCabecera}`,
         merchantlogo: 'https://img.bsginstitute.com/repositorioweb/img/logobsg-visa.svg',
         formbuttoncolor: '#eea341',
-        showamount: 'true',
-        countable:true,
-        action: 'https://localhost:44373/ProcesoPagoVisa/Index?IdTransaccion=' +
-            this.json.IdentificadorTransaccion,
-        showmethods:'true',
-        // paymentMethod:'tarjeta',
-        // paymentMethod:'card',
-        paymentMethods:'false',
-        payments:false,
-        yape:'false',
-        qr:'false',
-        paymentqr:false,
-
-
-        complete: (params:any) => {
-          console.log(params)
-          alert(JSON.stringify(params)); // Manejar la respuesta de Visa aquí
+        showamount: 'false',
+        countable: false,
+        cardholdername: `${this.resultNiubiz.registroAlumno.nombre}`,
+        cardholderlastname: `${this.resultNiubiz.registroAlumno.apellido}`,
+        cardholderemail: `${this.resultNiubiz.registroAlumno.correo}`,
+        usertoken: `${this.resultNiubiz.registroAlumno.idAlumno}`,
+        action: `https://localhost:44373/ProcesoPagoVisa/Index?IdTransaccion=${this.json.IdentificadorTransaccion}`,
+        complete: (params: any) => {
+          console.log(params);
+          alert(JSON.stringify(params));
         },
       });
-      VisanetCheckout.open(); // Abre el formulario de pago
+      VisanetCheckout.open();
     } catch (error) {
       console.error('Error al abrir el formulario de Visa Checkout:', error);
     }
   }
 
   FormatoMilesDecimales(num: number): string {
-    const parts = Number(num).toFixed(2).split('.');
-    let integerPart = parts[0];
-    const decimalPart = parts[1];
-    integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-    return integerPart + '.' + decimalPart;
+    const [integerPart, decimalPart] = Number(num).toFixed(2).split('.');
+    return integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '.' + decimalPart;
   }
-  RegresarPasarela(){
-    this._router.navigate(['/AulaVirtual/MisPagos/'+this.IdMatriculaCabecera+'/'+this.IdPasarelaPago]);
+
+  RegresarPasarela(): void {
+    this._router.navigate(['/AulaVirtual/MisPagos/', this.IdMatriculaCabecera, this.IdPasarelaPago]);
   }
-  ObtenerTarjetasMedioPago() {
+
+  ObtenerTarjetasMedioPago(): void {
     this._MedioPagoActivoPasarelaService
       .MedioPagoPasarelaPortalCronograma(this.IdMatriculaCabecera)
       .pipe(takeUntil(this.signal$))
       .subscribe({
-        next: (x:any) => {
-          this.tarjetas = x;
-          this.tarjetas.forEach((e: any) => {
-            e.img = this._t.GetTarjeta(e.medioCodigo);
-          });
+        next: (response) => {
+          this.tarjetas = response.map((tarjeta: any) => ({
+            ...tarjeta,
+            img: this._t.GetTarjeta(tarjeta.medioCodigo),
+          }));
           console.log('Tarjetas por alumno', this.tarjetas);
-        },
-        complete: () => {
-          // this.ObtenerInformacionPagoLocal()
         },
       });
   }
