ee4173565aa208cbba8961e1515e5362f5b0992e
Max Mantilla Rodríguez <mmantilla@bsginstitute.com>
2024-11-06 12:24:47 -0500

    correcciones ancho detalle

--- src/app/aula-virtual/pago/pago-medio-pago/detalle-pago-peru-izipay/detalle-pago-peru-izipay.component.ts
+++ src/app/aula-virtual/pago/pago-medio-pago/detalle-pago-peru-izipay/detalle-pago-peru-izipay.component.ts
@@ -179,8 +179,8 @@ export class DetallePagoPeruIzipayComponent
     if (typeof boton != 'undefined' && boton != null && boton.length > 0)
       boton[0].setAttribute(
         'style',
-        'background-color: #F8893F;color: white;padding: 0 6px 0 6px;margin: 0px;min-width: 88px;border-radius: 3px;font-size: 14px;' +
-          'text-align: center;text-transform: uppercase;text-decoration:none;border: none;outline: none;box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26);&:hover {background-color: #b26a3b}'
+        'height: 2rem;width: 10rem;;background-color: #283C7E;color: white;padding: 0 6px 0 6px;margin: 0px;min-width: 88px;border-radius: 10px;font-size: 14px;' +
+          'text-align: center;text-transform: uppercase;text-decoration:none;border: none;outline: none;box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26);&:hover {background-color: #3A52A3 }'
       );
 
     var botonPago = document.getElementsByClassName('kr-payment-button');
