1a8fbb9dd78b57a24a0d1da68bfe84abcfc886f1
Jorge_Gamero <jgamerom@bsginstitute.com>
2025-02-03 20:43:16 -0500

    Avance interfaz configuración resumen grabaciones [1uStVm0sMoX0d5gt9OFrvwtBJ68tfji4RWq2XWk2z2Ec]

--- src/app/aula-virtual/aula-virtual.module.ts
+++ src/app/aula-virtual/aula-virtual.module.ts
@@ -216,6 +216,7 @@ import { DetallePagoChileMercadopagoComponent } from './pago/pago-medio-pago/det
 import { DetallePagoColombiaWompiComponent } from './pago/pago-medio-pago/detalle-pago-colombia-wompi/detalle-pago-colombia-wompi.component';
 import { DetallePagoColombiaPayuComponent } from './pago/pago-medio-pago/detalle-pago-colombia-payu/detalle-pago-colombia-payu.component';
 import { ResultadoPagoNiubizComponent } from './resultado-pago-niubiz/resultado-pago-niubiz.component';
+import { CursoConfiguracionResumenComponent } from './curso/curso-configuracion-resumen/curso-configuracion-resumen/curso-configuracion-resumen.component';
 
 
 @NgModule({
@@ -433,6 +434,7 @@ import { ResultadoPagoNiubizComponent } from './resultado-pago-niubiz/resultado-
     DetallePagoColombiaWompiComponent,
     DetallePagoColombiaPayuComponent,
     ResultadoPagoNiubizComponent,
+    CursoConfiguracionResumenComponent,
 
   ],
   imports: [
---
+++ src/app/aula-virtual/curso/curso-configuracion-resumen/curso-configuracion-resumen/curso-configuracion-resumen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curso-configuracion-resumen',
+  templateUrl: './curso-configuracion-resumen.component.html',
+  styleUrls: ['./curso-configuracion-resumen.component.scss']
+})
+export class CursoConfiguracionResumenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
