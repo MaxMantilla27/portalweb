1097f6144a9069b19993929918e657f4242012c7
JNPG1999 <jeremy_jair_2015@hotmail.com>
2025-02-12 15:12:58 -0500

    estilos datos claves

--- src/app/Public/programas-detalle/programas-detalle.component.ts
+++ src/app/Public/programas-detalle/programas-detalle.component.ts
@@ -304,26 +304,24 @@ export class ProgramasDetalleComponent implements OnInit ,OnDestroy{
       if (element) {
         const elementTop = element.getBoundingClientRect().top + window.scrollY;
         const elementBottom = elementTop + element.offsetHeight;
-        if (offset >= elementTop - 130 && offset < elementBottom - 130 && this.innerWidth>1485) { // Ajusta el valor según la altura de la barra de navegación fija  
+        if (offset >= elementTop - 130 && offset < elementBottom - 130 && this.innerWidth>=1000) { // Ajusta el valor según la altura de la barra de navegación fija  
           this._HelperServiceP.enviarScrollHeaderPrograma(offset >= elementTop - 130);
           this.activateSeccion = seccion.ref;
           if (this.activateSeccion === 'inversion') {
             const absoluteFormulario = document.getElementById('absoluteFormulario') as HTMLElement
             absoluteFormulario.style.display = 'none';
           }
-        } else if (seccion.ref === 'objetivos' && offset < elementTop - 130 && this.innerWidth>1485) {
+        } else if (seccion.ref === 'objetivos' && offset < elementTop - 130 && this.innerWidth>=1000) {
           this._HelperServiceP.enviarScrollHeaderPrograma(false);
         } 
       }
-
-
       });
     }
 
   @HostListener('window:resize')
   onResize() {
     this.innerWidth = window.innerWidth; 
-    if (this.innerWidth <= 1485) {
+    if (this.innerWidth < 1000) {
       this._HelperServiceP.enviarScrollHeaderPrograma(false);
     }
   }
