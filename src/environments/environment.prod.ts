2b07d2e0f47969a17f591e9ec7954dcc7a1252d3
Max Mantilla Rodríguez <mmantilla@bsginstitute.com>
2025-02-15 11:25:12 -0500

    Actualización de rutas de integra y signalr

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,12 @@ export const environment = {
   production: true,
   url_api:'https://api-portalweb-prototipo.bsginstitute.com/api/',
   // url_api_integra:'https://integrav4-servicios.bsginstitute.com/api/',
-  url_api_integra:'https://integrav4-prototipo.bsginstitute.com/api/',
+  // url_api_integra:'https://integrav4-prototipo.bsginstitute.com/api/',
+  url_api_integra:'https://integrav4-prepublicacion-servicios.bsginstitute.com/api/',
   // url_portal:'https://bsginstitute.com/',
   url_portal:'https://prototipo.bsginstitute.com/',
-  url_signal:'https://signalr-prototipo.bsginstitute.com/',
+  // url_signal:'https://signalr-prototipo.bsginstitute.com/',
+  url_signal:'https://integrav4-signalrcore.bsginstitute.com/',
   //url_signal:'https://localhost:7120/'
   // url_signal:'https://localhost:7120/',
   // url_api_integra_v5:'https://localhost:7288/api'
